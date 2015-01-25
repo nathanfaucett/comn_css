c0493e19346ccced97f7e18680b67d9cda27b3ca
nathanfaucett <nathanfaucett@gmail.com>
2015-01-25 09:55:30 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-var map = require("map"),
-    forEach = require("for_each"),
-    filePath = require("file_path"),
+var filePath = require("file_path"),
     resolve = require("resolve"),
     parseDependencyTree = require("parse_dependency_tree");
 
@@ -9,7 +7,7 @@ module.exports = comnCSS;
 
 
 function comnCSS(index, options) {
-    var graph, array, order;
+    var graph;
 
     options = options || {};
     options.exts = ["less", "css"];
@@ -20,46 +18,31 @@ function comnCSS(index, options) {
     options.beforeParse = beforeParse;
 
     graph = parseDependencyTree(index, options);
-    hash = graph.hash;
-    array = graph.array;
-    order = 1;
 
-    forEach(array, function(dependency) {
-        var parentDir = filePath.dir(dependency.fullPath);
-
-        dependency.order = dependency.order || order++;
-
-        dependency.content = dependency.content.replace(graph.reInclude, function(match, fnType, dependencyPath) {
-            var opts = resolve(dependencyPath, parentDir, options),
-                id = opts && (opts.moduleName ? opts.moduleName : opts.fullPath) || false,
-                dependency = id ? hash[id] : null;
-
-            if (dependency) {
-                dependency.order = dependency.order || order++;
-            }
-
-            return "";
-        });
-    });
-
-    array.sort(sortFn);
-
-    return render(array);
+    return replaceImports(graph.array[0], graph);
 }
 
-function sortFn(a, b) {
-    return b.order - a.order;
+function replaceImports(dependency, graph) {
+    var options = graph.options,
+        hash = graph.hash,
+        parentDir = filePath.dir(dependency.fullPath);
+
+    dependency.used = true;
+
+    return dependency.content.replace(graph.reInclude, function(match, fnType, dependencyPath) {
+        var opts = resolve(dependencyPath, parentDir, options),
+            id = opts && (opts.moduleName ? opts.moduleName : opts.fullPath) || false,
+            dep = id ? hash[id] : null;
+
+        if (dep && !dep.used) {
+            return replaceImports(dep, graph);
+        }
+
+        return "";
+    });
 }
 
 function beforeParse(content, cleanContent, dependency) {
     dependency.content = content;
     return cleanContent;
-}
-
-function render(dependencies) {
-    return map(dependencies, renderDependency).join("\n");
-}
-
-function renderDependency(dependency) {
-    return dependency.content;
 }
